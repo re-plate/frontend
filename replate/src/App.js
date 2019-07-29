f0496230a2cee680e4c30424148654792e0b62d3
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-29 11:51:45 -0700

    structured app.js

--- replate/src/App.js
+++ replate/src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [teamArray, setTeamArray] = useState([
+    {id: "1", name: "Scooby Doo",email: "woof@gmail.com",requests: "...",imgUrl:"https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/20992941_10156238556155961_119237620566999832_n.jpg?_nc_cat=1&_nc_oc=AQl1EFkKWnp4l9SRuNIQaRVkxAF_PjNwnpTd0ykobscIw61sB2XFzUf4rjYflhOHuwA&_nc_ht=scontent-dfw5-1.xx&oh=4a564cede59d7d420c752712f701cce8&oe=5DA3195E"},
+  ]);
+
+  const [idCount, setIdCount] = useState(3);
+  const [memberToEdit, switchEditMember] = useState();
+  const [isEditing, setEdit] = useState(false);
+  
+  const addToMembers = member => {
+    isEditing
+      ? setTeamArray(member)
+      : setTeamArray([...teamArray, { ...member, id: idCount }]);
+  };
+
+  const editTrue = id => {
+    setEdit(true);
+    switchEditMember(id);
+  };
+
+  const editFalse = () => {
+    setEdit(false);
+    switchEditMember(null);
+  };
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <button className="requestButton">View open requests</button>
+      {teamArray.map((member) => 
+            <div className = "card">
+            <img src={member.imgUrl}/>
+            <div className = "textContainer">
+              <h1>{member.name}</h1>
+              <p>{member.email}</p>
+              <p>{member.requests}</p>
+            </div>
+            <button className="editButton" onClick={() => editTrue(member.id)}>Edit</button>
+          </div>
+        )}
+        <button className="requestButton">View open requests</button>
+
     </div>
+
   ); 
 }
 
