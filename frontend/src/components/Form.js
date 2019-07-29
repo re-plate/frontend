92df6f4ff9b8e03f2b556dcbbf38c2f27d7aada1
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-07-29 14:17:35 -0700

    added Form labels

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -8,13 +8,18 @@ const Form = props => {
       <input type="text" name="edit" />
       <div className="form-container">
         <form onSubmit={props.handleSubmit}>
-          <label>Name</label>
-          <input type="text" name="name" required />
+          <h1>Donor Sign Up</h1>
+          <label>Company Name</label>
+          <input type="text" name="company name" required />
+          <label>Company Contact Name</label>
+          <input type="text" name="company name" required />
+          <label>Phone</label>
+          <input type="text" name="phone" required />
           <label>Email</label>
           <input type="text" name="email" required />
-          <label>Role</label>
+          <label>Password</label>
           <input type="text" name="role" required />
-          <button> Submit </button>
+          <button> Sign Up </button>
         </form>
       </div>
     </div>
