a49960aed9910f4f0cf9d448254325ce790d7307
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-29 12:13:23 -0700

    fixed initial edit index

--- replate/src/App.js
+++ replate/src/App.js
@@ -7,7 +7,7 @@ function App() {
     {id: "1", name: "SpongeBob",email: "cook@krustykrab.com",requests: "...",imgUrl:"https://vignette.wikia.nocookie.net/spongebob/images/d/d7/SpongeBob_stock_art.png/revision/latest?cb=20190604110949"},
   ]);
 
-  const [idCount, setIdCount] = useState(3);
+  const [idCount, setIdCount] = useState(2);
   const [volunteerToEdit, switchEditVolunteer] = useState();
   const [isEditing, setEdit] = useState(false);
   
